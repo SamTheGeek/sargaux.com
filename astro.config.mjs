4379a6e925aa10b05a96505fe524650f560cd67c
Sam Gross <SamTheGeek@users.noreply.github.com>
2026-02-03 23:45:24 -0500

    updating tests

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
+import node from '@astrojs/node';
 
 // https://astro.build/config
+const useNodeAdapter = process.env.ASTRO_ADAPTER === 'node';
+
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: useNodeAdapter ? node({ mode: 'standalone' }) : netlify(),
   server: {
     port: 1213
   }
